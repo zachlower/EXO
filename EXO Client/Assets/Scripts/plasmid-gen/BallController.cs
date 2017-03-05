a36701269368a623ddc51645f33697e1b738c243
Zach Lower <zlower@usc.edu>
2017-03-05 15:24:29 -0800

    starting work on plasmid generation

---
+++ EXO Client/Assets/Scripts/plasmid-gen/BallController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    public float topSpeed = 5.0f;
+
+    private Rigidbody2D rb;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 input = new Vector2();
+        input.x = Input.GetAxis("Horizontal");
+        input.y = Input.GetAxis("Vertical");
+
+        rb.AddForce(input * topSpeed);
+
+    }
+}
