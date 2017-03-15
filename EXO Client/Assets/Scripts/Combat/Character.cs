6644fe0e3f202ca9608220adfacd6cdeb9af0838
Zach Lower <zlower@usc.edu>
2017-03-15 14:24:45 -0700

    starting to make some test objects for combat

--- EXO Client/Assets/Scripts/Combat/Character.cs
+++ EXO Client/Assets/Scripts/Combat/Character.cs
@@ -10,14 +10,15 @@ public class Character : MonoBehaviour {
 
 
     // character health
-    public int currentHealth { get; private set; }
-    public int maxHealth { get; private set; }
+    public int currentHealth { get; protected set; }
+    public int maxHealth { get; protected set; }
 
     // durational effects (have impact each turn until they expire)
     public List<Effect> currentEffects;
+    // abilities that a character has
+    public List<Ability> abilities;
 
 
-    
     // TODO: when applying effects, take into account defenses against various attack types and effect types
     // apply effect when character gets targeted by an ability
     public void ApplyEffect(Effect e, float powerModifier)
@@ -39,6 +40,14 @@ public class Character : MonoBehaviour {
         }
     }
 
+
+    // trigger durational effects every interval (TODO: interval?)
+    public IEnumerator DurationalEffectTrigger(float interval)
+    {
+        yield return new WaitForSeconds(interval);
+        if(currentEffects != null && currentEffects.Count != 0)
+            DurationalEffects();
+    }
     // apply durational effects
     private void DurationalEffects()
     {
---
+++ EXO Client/Assets/Scripts/Combat/Characters/Zachomorph.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zachomorph : Character {
+
+    /* example of a character 
+     */
+
+    private void Awake()
+    {
+        maxHealth = 100;
+        currentHealth = maxHealth; //TODO: carry this over from previous encounters
+
+        currentEffects = new List<Effect>();
+
+        abilities = new List<Ability>();
+            // TODO: add abilities to list
+
+
+    }
+}
